ffc5722418248c37fe8749e19cf3043ec78c1ca0
Bart <bartlankheet@gmail.com>
2026-03-08 19:29:44 +0100

    gifs

--- Set2_RD_system.py
+++ Set2_RD_system.py
@@ -130,7 +130,7 @@ def run_GS_simulation(F=0.035, k=0.065, Du=0.16, Dv=0.05, dt=1, filepath='sim_re
 # vvv Example usage vvv
 
 run_GS_simulation(
-    F=0.035, 
+    F=0.095, 
     k=0.065, 
     Du=0.16, 
     Dv=0.05, 
